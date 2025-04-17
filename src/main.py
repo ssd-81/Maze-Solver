1c42e35ab0dbb59d2a7374fb75676fca75a30a7c
Singh-Sagar <arcsagar.91@gmail.com>
2025-04-18 01:33:18 +0530

    window creation working

--- src/main.py
+++ src/main.py
@@ -4,3 +4,7 @@ from window import Window
 def main():
     win = Window(800, 600)
     win.wait_for_close()
+
+
+if __name__ == "__main__":
+    main()
--- src/window.py
+++ src/window.py
@@ -5,20 +5,19 @@ class Window:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        root = Tk.Tk()
-        root.title = "visualize-maze"  # a placeholder for now
-        # might need to recheck this
-        canvas = Canvas()
-        canvas.pack()
-        self.window_running = False
         self.__root = Tk()
+        self.__root.title("visualize-maze")
+        self.canvas = Canvas(self.__root, height=self.height, width=self.width)
+        self.canvas.pack()
+        self.window_running = True
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        root.update_idletasks()
-        root.update()
+        self.__root.update_idletasks()
+        self.__root.update()
 
     def wait_for_close(self):
+        self.window_running = True
         while self.window_running:
             self.redraw()
 
