27f37b2ccc23b7b7f2a2478dd2cb73290f2f3313
Singh-Sagar <arcsagar.91@gmail.com>
2025-05-06 19:02:48 +0530

    cell draw and line between cells

--- src/draw.py
+++ src/draw.py
@@ -16,12 +16,51 @@ class Line:
         fill=fill_color, width=2)
 
 class Cell:
-    def __init__(self):
+    def __init__(self, x1, x2, y1, y2, win):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self._x1 = None # I don't know 
-        self._x2 = None 
-        
+        self._x1 = x1  
+        self._x2 = x2 
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
 
+    def draw(self):
+        if self.has_left_wall:
+            left_wall = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(left_wall, "black")
+        if self.has_right_wall:
+            right_wall = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(right_wall, "black")
+        if self.has_top_wall:
+            top_wall = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(top_wall, "black")
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(bottom_wall, "black")
+    
+    def draw_move(self, to_cell, undo=False):
+        self_center = Point((self._x1 + self._x2)/2, (self._y1 + self._y2)/2)
+        to_cell_center = Point((to_cell._x1 + to_cell._x2)/2, (to_cell._y1 + to_cell._y2)/2)
+        line = Line(self_center, to_cell_center)
+        if not undo:
+            self._win.draw_line(line, "red")
+        else:
+            self._win.draw_line(line, "gray")
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows 
+        self._num_cols = num_cols 
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+    
+    def _create_cells(self):
+        for rows in self._num_rows:
+            for cell in self._num_cols:
+                cell = Cell()
--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from window import Window
-from draw import Point, Line
+from draw import Point, Line, Cell
 
 
 def main():
     win = Window(800, 600)
-    line = Line(Point(100, 200), Point(300, 800))
-    win.draw_line(line, "black")
+
+    # line = Line(Point(100, 200), Point(300, 800))
+    # win.draw_line(line, "black")
+
+    test_cell_1 = Cell(100, 200, 200, 300, win)
+    # test_cell_1.has_right_wall = False
+    test_cell_2 = Cell(500, 700, 350, 450, win)
+    # test_cell_2.has_left_wall = False
+    test_cell_1.draw()
+    test_cell_2.draw()
+    test_cell_1.draw_move(test_cell_2)
     win.wait_for_close()
 
 
